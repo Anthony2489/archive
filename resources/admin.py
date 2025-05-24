d1a5e7ed1b70e426b04c6d426de061b327ea889d
Anthony2489 <antonyf2047@gmail.com>
2025-05-24 20:27:23 +0300

    added Resources models

--- resources/admin.py
+++ resources/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+# Register your models here
+from .models import Departments, Courses, CourseGroup, Assignments, AssignmentSubmissions, resources
+
+admin.site.register(Departments)
+admin.site.register(Courses)
+admin.site.register(CourseGroup)
+admin.site.register(Assignments)
+admin.site.register(AssignmentSubmissions)
+admin.site.register(resources)
+                    
